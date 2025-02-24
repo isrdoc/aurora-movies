089388f9fb05261f27e8912306e0e59f3f16d94b
IgorSrdoc <igor@srdoc.si>
2025-02-24 13:55:44 +0100

    feat: shadcn ui components

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,23 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Aurora Movies</h1>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Content />
+    </ThemeProvider>
+  );
+}
+
+function Content() {
+  return (
+    <div className="w-full">
+      <div className="flex items-center justify-between w-full">
+        <h1 className="text-2xl font-bold">Aurora Movies</h1>
+        <ModeToggle />
+      </div>
+      <Button>Example Button</Button>
     </div>
   );
 }
