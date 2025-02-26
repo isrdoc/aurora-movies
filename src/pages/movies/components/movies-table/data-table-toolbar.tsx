f26f6325c9690a40cca72be62097221eaf0868f6
IgorSrdoc <igor@srdoc.si>
2025-02-26 12:47:35 +0100

    chore: change search placeholder

--- src/pages/movies/components/movies-table/data-table-toolbar.tsx
+++ src/pages/movies/components/movies-table/data-table-toolbar.tsx
@@ -18,7 +18,7 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter movies..."
+          placeholder="Search movies..."
           value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
           onChange={(event) => {
             onSearchChange(event.target.value);
